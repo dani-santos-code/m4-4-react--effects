4607f85aa09aac85fc973721462920d6c7a6ebf7
Dani Santos <santos.adaniele@gmail.com>
2020-03-10 17:06:27 -0400

    removes tabIndex from button

--- src/components/Game.js
+++ src/components/Game.js
@@ -79,7 +79,7 @@ const Game = () => {
           <Total>{numCookies} cookies</Total>
           <strong>{totalCookiesPerSecond}</strong> cookies per second
         </Indicator>
-        <Button tabIndex={-1} onClick={() => setNumCookies(numCookies + 1)}>
+        <Button onClick={() => setNumCookies(numCookies + 1)}>
           <Cookie src={cookieSrc} />
         </Button>
       </GameArea>
